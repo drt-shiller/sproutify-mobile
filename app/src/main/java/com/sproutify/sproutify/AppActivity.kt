7b790b89eedd74cf0654f70c56ec17e214369148
drtshiller <drtshiller@hotmail.com>
2025-06-14 18:41:03 +0200

    [build] add hilt configuration

--- app/src/main/java/com/sproutify/sproutify/AppActivity.kt
+++ app/src/main/java/com/sproutify/sproutify/AppActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import com.sproutify.auth.landing.LandingScreen
 import com.sproutify.designsystem.theme.SprtTheme
+import com.sproutify.sproutify.ui.SprtApp
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class AppActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
---
+++ app/src/main/java/com/sproutify/sproutify/SprtApplication.kt
@@ -0,0 +1,9 @@
+package com.sproutify.sproutify
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SprtApplication : Application() {
+
+}
